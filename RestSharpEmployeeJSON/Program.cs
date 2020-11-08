d09f2aa19d20efcbdd2f77bd41e2ea511c4d217d
Akash <akashmane12598@gmail.com>
2020-11-08 14:42:23 +0530

    [Akash] Add. Master Code

---
+++ RestSharpEmployeeJSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestSharpEmployeeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to API testing using RESTSharp Library!");
+        }
+    }
+}
