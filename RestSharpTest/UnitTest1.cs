9784558fdfbd416f79cd6538044ccdf7ae6e4a79
Akash <akashmane12598@gmail.com>
2020-11-09 00:37:55 +0530

    [Akash] Add. Update Salary of Employee into JSON server UC4

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace RestSharpTest
@@ -45,7 +46,7 @@ namespace RestSharpTest
 
             List<EmployeePayroll> records = JsonConvert.DeserializeObject<List<EmployeePayroll>>(response.Content);
 
-            Assert.AreEqual(10, records.Count);
+            Assert.AreEqual(11, records.Count);
 
             foreach (EmployeePayroll employee in records)
             {
@@ -93,5 +94,100 @@ namespace RestSharpTest
             Console.WriteLine(response.Content);
 
         }
+
+
+        /*public void AddMultipleEmployeesIntoJSONServer()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST, DataFormat.Json);
+
+            List<object> input = new List<object>();
+
+            var employees = new[] { new { name = "Rohit", salary = "100000" }, new { name = "Suresh", salary = "80000" } };
+
+            
+
+            //This method sets content type to application/json and serializes the object to a JSON string.
+            //request.AddJsonBody(new { name = "Rohit", salary = "100000" });
+            //request.AddJsonBody(new { name = "Suresh", salary = "80000" });
+
+            //EmployeePayroll[] employees = new EmployeePayroll[]{ new EmployeePayroll{ name = "Rohit", salary = "100000" } , new EmployeePayroll { name = "Suresh", salary = "80000" } };
+
+            input.Add(employees);
+
+            request.AddJsonBody(input);
+
+            /*JObject jObject1 = new JObject();
+            jObject1.Add("name","Rohit");
+            jObject1.Add("salary", "100000");
+
+            request.AddParameter("application/json", jObject1, ParameterType.RequestBody);
+
+            JObject jObject2 = new JObject();
+            jObject2.Add("name","Suresh");
+            jObject2.Add("salary","80000");*/
+
+            //request.AddParameter("application/json", jObject2, ParameterType.RequestBody);
+
+
+            //request.AddJsonBody(employee1);
+
+            /*string json = JsonConvert.SerializeObject(employees);
+
+            request.AddParameter(json, "application/json", ParameterType.RequestBody);*/
+
+            //IRestResponse response = restClient.Execute(request);
+
+            //var records = JsonConvert.DeserializeObject<object>(response.Content);
+
+            //List<object> records = JsonConvert.DeserializeObject<List<object>>(response.Content);
+
+            /*Assert.AreEqual("Rohit", records[0].name);
+
+            Assert.AreEqual("100000", records[0].salary);
+
+            Assert.AreEqual("Suresh", records[1].name);
+
+            Assert.AreEqual("80000", records[1].salary);*/
+
+            /*Assert.AreEqual("Rohit" ,records.Where(x => x.name.Equals("Rohit")).Select(x=>x.name));
+
+            Assert.AreEqual("100000", records.Where(x => x.name.Equals("Rohit")).Select(x=>x.salary));
+
+            Assert.AreEqual("Suresh", records.Where(x => x.name.Equals("Suresh")).Select(x => x.name));
+
+            Assert.AreEqual("80000", records.Where(x => x.name.Equals("Suresh")).Select(x => x.salary));*/
+
+            //Console.WriteLine(response.Content);
+
+        //}
+
+        /// <summary>
+        /// TC4 Update Salary of Employee
+        /// </summary>
+        [TestMethod]
+        public void UpdateSalaryIntoJSONServer()
+        {
+            RestRequest request = new RestRequest("/employees/11", Method.PUT);
+
+            JObject jObject = new JObject();
+            jObject.Add("name", "Tanmay");
+            jObject.Add("salary", "30000");
+
+            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+
+            IRestResponse response = restClient.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+
+            EmployeePayroll employee = JsonConvert.DeserializeObject<EmployeePayroll>(response.Content);
+
+            Assert.AreEqual("Tanmay", employee.name);
+
+            Assert.AreEqual("30000", employee.salary);
+
+            Console.WriteLine(response.Content);
+
+        }
+
     }
 }
