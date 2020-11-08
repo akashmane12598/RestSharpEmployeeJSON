46bcf4f4830afc30fc6a2623e7e996952a43aaa2
Akash <akashmane12598@gmail.com>
2020-11-08 16:23:32 +0530

    [Akash] Add. Retrieved EmployeePayroll Data from JSON server UC1

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -1,13 +1,62 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
 
 namespace RestSharpTest
 {
+    public class EmployeePayroll
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string salary { get; set; }
+
+    }
+
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        RestClient restClient;
+
+        /// <summary>
+        /// Creates Client and by passing root URL, establishes connection with the server
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
         {
+            restClient = new RestClient("http://localhost:4000");
+        }
+
+        /// <summary>
+        /// TC1 Retrieves All employee details by GET method
+        /// </summary>
+        [TestMethod]
+        public void RetrieveEmployeeData()
+        {
+            IRestResponse response = GetEmployeeDetails();
+
+            List<EmployeePayroll> records = JsonConvert.DeserializeObject<List<EmployeePayroll>>(response.Content);
+
+            Assert.AreEqual(9, records.Count);
+
+            foreach (EmployeePayroll employee in records)
+            {
+                Console.WriteLine("EmployeeID: "+employee.id+", EmployeeName: "+employee.name+", Salary: "+employee.salary);
+            }
+        }
+
+        public IRestResponse GetEmployeeDetails()
+        {
+            //Creates GET request for accessing "/employees", for sending the request to the server
+            RestRequest request = new RestRequest("/employees", Method.GET);
+
+            //Sends the request to the server and in return gets the response which is collected by IRestResponse 
+            IRestResponse response = restClient.Execute(request);
+
+            return response;
         }
     }
 }
