209b8365ddfe9809a87269714521794978be853c
Akash <akashmane12598@gmail.com>
2020-11-08 17:21:40 +0530

    [Akash] Add. Add a new EmployeePayroll Data into JSON server UC2

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -1,8 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace RestSharpTest
 {
@@ -38,9 +40,12 @@ namespace RestSharpTest
         {
             IRestResponse response = GetEmployeeDetails();
 
+            //Checks whether the data is retrieved or not(200 is statuscode for retrieval)
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+
             List<EmployeePayroll> records = JsonConvert.DeserializeObject<List<EmployeePayroll>>(response.Content);
 
-            Assert.AreEqual(9, records.Count);
+            Assert.AreEqual(10, records.Count);
 
             foreach (EmployeePayroll employee in records)
             {
@@ -57,6 +62,36 @@ namespace RestSharpTest
             IRestResponse response = restClient.Execute(request);
 
             return response;
+        }
+
+        /// <summary>
+        /// TC2 Add a new Employee data into JSON server
+        /// </summary>
+        [TestMethod]
+        public void AddaEmployeeIntoJSONServer()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+
+            //Creating new Emp Json data for adding into server
+            JObject jObject = new JObject();
+            jObject.Add("name","Kunal");
+            jObject.Add("salary","20000");
+
+            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+
+            IRestResponse response = restClient.Execute(request);
+
+            //StatusCode for Adding data is 201
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+
+            EmployeePayroll employees = JsonConvert.DeserializeObject<EmployeePayroll>(response.Content);
+
+            Assert.AreEqual("Kunal", employees.name);
+
+            Assert.AreEqual("20000", employees.salary);
+
+            Console.WriteLine(response.Content);
+
         }
     }
 }
